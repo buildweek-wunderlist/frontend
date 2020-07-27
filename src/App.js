aac45f4a0004d600bd01484d73258ccaff27f096
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-07-27 16:35:25 -0300

    added basic structure for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Logon from './components/Logon'
+import Register from './components/Register'
 
 function App() {
   return (
     <div>
+      <Logon />
+      <Register />
     </div>
   );
 }
