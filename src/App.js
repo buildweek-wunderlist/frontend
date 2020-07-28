37cd59d0575ad2a02ee770b8286cca5ef62c9466
unknown <ali-golji@lambdastudents.com>
2020-07-27 18:25:48 -0700

    register and app component skeletons complete.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Logon from './components/Logon'
-import Register from './components/Register'
+import Logon from './components/Logon';
+import Register from './components/Register';
+import axios from 'axios';
+import registerSchema from './validation/registerSchema';
+import * as yup from 'yup';
+
+
+
+const initialFormValues = {
+  username: '',
+  password: '',
+}
+
+const initialFormErrors = {
+  username: '',
+  password: '',
+}
+
 
 function App() {
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+
+
+  const submit = () => {
+    axios.post('https://jmesull-wunderlist.herokuapp.com/createnewuser', formValues)
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(error => {
+
+      })
+  }
+
+  const update = ( name, value ) => {
+    yup.reach(registerSchema, name)
+    .validate(value)
+    .then(valid => {
+      setFormErrors({
+        ...formErrors,
+        [name]: ''
+      })
+    })
+    .catch(error => {
+      console.log(error.errors)
+      setFormErrors({
+        ...formErrors,
+        [name]: error.errors[0]
+      })
+    })
+
+    const newUser = {
+      ...formValues,
+      [name]: value,
+    }
+    setFormValues(newUser)
+  }
+
+
   return (
     <div>
       <Logon />
-      <Register />
+      <Register
+        values={formValues}
+        errors={formErrors}
+        submit={submit}
+        update={update}
+      />
     </div>
   );
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,72 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+// const StyledRegister = styled.div`
+
+// `
+
+//END OF STYLES
+
+
+
 
 const Register = props => {
+
+    const {
+        values,
+        errors,
+        submit,
+        update
+    } = props
+
+    const changeHandler = (e) => {
+        e.persist(e)
+        const name = e.target.name
+        const value = e.target.value
+        update(name, value)
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        submit();
+
+    }
+
+
     return (
         <div>
+            <form onSubmit={submitHandler} className='register-form'>
+                <h2>User Registration</h2>
+
+                <label htmlFor='username'>
+                    Username:
+                    <input
+                        type='text'
+                        name='username'
+                        placeholder='Create New Username'
+                        value={values.username}
+                        onChange={changeHandler}
+                    />
+                </label>
+
+                <label htmlFor='password'>
+                    Password
+                    <input
+                        type='text'
+                        name='password'
+                        placeholder='Create New Password'
+                        value={values.password}
+                        onChange={changeHandler}
+                    />
+                </label>
+                <div className='errors'>
+                    <div>{errors.username}</div>
+                    <div>{errors.password}</div>
+                </div>
+
+                <button>Register!</button>
+            </form>
 
         </div>
     )
