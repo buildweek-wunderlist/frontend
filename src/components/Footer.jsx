73654faa27ea5550ae3bb6b02f2e122f2e94a3c5
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-07-28 14:57:21 -0300

    added basic styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from "react"
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+  background-color: #81b29a;
+  height: 10rem;
+  display: flex;
+  justify-content:center;
+  align-items:center;
+`
 
 const Footer = () => {
   return (
-    <div>
+    <StyledFooter>
       <h3>Wonderlist 2.0 by Lambda School</h3>
-    </div>
+    </StyledFooter>
   )
 }
 
